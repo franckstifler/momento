82482265ab5552c71460ffbbdcd9eed4db761975
franckstifler <francktchowa@gmail.comm>
2018-10-06 21:52:25 +0100

    Switched to parcel as bundle handler.

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,9 @@ config :momento, MomentoWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+  watchers: [node: ["node_modules/parcel-bundler/bin/cli.js",
+                    "watch", "js/app.js",
+                    "--out-dir", "../priv/static/assets",
                     cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
