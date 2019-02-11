9dfa604f56048ed5cd727cc90e6f7e81dfc5dd4e
franckstifler <francktchowa@gmail.comm>
2019-02-11 10:46:23 +0100

    Added test for deleting comment.

--- test/momento_web/schema/mutation/create_comment_test.exs
+++ test/momento_web/schema/mutation/create_comment_test.exs
@@ -31,8 +31,43 @@ defmodule MomentoWeb.Schema.Mutation.CreateComment do
     response = post(conn, "/api", %{query: @query, variables: comment})
 
     assert %{
+             "data" => %{
+               "createComment" => data
+             }
+           } = json_response(response, 200)
+
+    assert is_nil(data["errors"])
+    assert data["comment"]["comment"] == comment.comment
+  end
+
+  @query """
+    mutation($comment_id: Int!) {
+      deleteComment(comment_id: $comment_id) {
+        errors {
+          message
+        }
+        comment {
+          id
+          comment
+        }
+      }
+    }
+  """
+
+  test "Delete an existing comment" do
+    user = Factory.create_user()
+    slice = Factory.create_slice()
+
+    {:ok, comment} =
+      Momento.Media.create_comment(user, %{comment: "This is a comment", slice_id: slice.id})
+
+    conn = put_auth_header(build_conn(), user)
+
+    response = post(conn, "/api", %{query: @query, variables: %{comment_id: comment.id}})
+
+    assert %{
       "data" => %{
-        "createComment" => data
+        "deleteComment" => data
       }
     } = json_response(response, 200)
 
